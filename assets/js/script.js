64b4ebffd2a0f39963d7eafe034ebcadfbc7ba8d
Amanda <daikiraidemodaisuki@gmail.com>
2019-01-19 18:09:19 -0800

    updated style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-$(".date").text((new Date()).getFullYear());
+function RemoveGrayScale(x) {
+    var background_img = document.getElementsByClassName("carousel-img");
+    background_img[x].classList.remove("gray");
+}
+
+function AddGrayScale(x) {
+    var background_img = document.getElementsByClassName("carousel-img");
+    background_img[x].classList.add("gray");
+}
