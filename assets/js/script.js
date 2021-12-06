4c56d6681a4300310f136e4d48fcad36ea32404f
amanda pan <daikiraidemodaisuki@gmail.com>
2021-12-05 18:57:06 -0800

    updated portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 function RemoveGrayScale(x) {
     var background_img = document.getElementsByClassName("carousel-img");
-    background_img[x].classList.remove("gray");
+    //background_img[x].classList.remove("gray");
+    background_img[x].style.filter = "grayscale(0)";
+    background_img[x].style.transition = "all 1s ease";
 }
 
 function AddGrayScale(x) {
     var background_img = document.getElementsByClassName("carousel-img");
-    background_img[x].classList.add("gray");
+    //background_img[x].classList.add("gray");
+    background_img[x].style.filter = "grayscale(100%)";
+    background_img[x].style.transition = "all 1s ease";
 }
